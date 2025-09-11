02e61f840c5cb371c42117c5d6d351de0100b203
Oleg Komarov <oleg@blender.org>
2025-09-11 16:02:36 +0200

    BLENDER: Contributor Agreement Support
    New models ContributorAgreement and SignedContributorAgreement are introduced.
    
    A new Settings > Contributor Agreements page is introduced.
    The page allows a user to review the text of existing contributor agreements
    and to sign any of them.
    
    A new /api/v1/users/{username}/contributor-agreements/{slug} endpoint allows to
    check if a user has signed a contributor agreement.
    This api endpoint can be used by a gitea action workflow, e.g. like this:
    
    ```
        name: contributor agreement check
    
        on:
          pull_request_target:
            types: [opened, ready_for_review, reopened, review_requested]
    
        env:
          CONTRIBUTOR_AGREEMENT: ...
    
        jobs:
          check_api:
            runs-on: ...
            steps:
              - run: echo "Checking $CONTRIBUTOR_AGREEMENT for ${{ gitea.event.pull_request.user.login }}"
              - run: result=$(curl --show-error --silent "$GITHUB_API_URL/users/${{ gitea.event.pull_request.user.login }}/contributor-agreements/$CONTRIBUTOR_AGREEMENT"); if [ "$result" == "OK" ]; then exit 0; else echo $result; exit 1; fi
    ```
    
    cleanup signed agreements on user deletion
    
    Admin UI for listing/search and batch signing
    
    always add a prefix for the batch sign comment

--- routers/api/v1/api.go
+++ routers/api/v1/api.go
@@ -1022,6 +1022,8 @@ func Routes() *web.Router {
 				}, reqSelfOrAdmin(), reqBasicOrRevProxyAuth())
 
 				m.Get("/activities/feeds", user.ListUserActivityFeeds)
+				// BLENDER: contributor agreement
+				m.Get("/contributor-agreements/{slug}", user.CheckContributorAgreement)
 			}, context.UserAssignmentAPI(), checkTokenPublicOnly(), individualPermsChecker)
 		}, tokenRequiresScopes(auth_model.AccessTokenScopeCategoryUser))
 

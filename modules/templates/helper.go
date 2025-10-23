18614dd2dc519bf9c9be1473869e330439284647
Brecht Van Lommel <brecht@blender.org>
2025-10-23 20:18:11 +0200

    BLENDER: Projects: button to show/hide issue details and closed issue
    Both are off by default. This is implemented fully on the frontend, so
    all issues and their details are still always loaded.

--- modules/templates/helper.go
+++ modules/templates/helper.go
@@ -40,6 +40,7 @@ func NewFuncMap() template.FuncMap {
 		"HTMLFormat":   htmlFormat,
 		"QueryEscape":  queryEscape,
 		"QueryBuild":   QueryBuild,
+		"JSEscape":     jsEscapeSafe,
 		"SanitizeHTML": SanitizeHTML,
 		"URLJoin":      util.URLJoin,
 		"DotEscape":    dotEscape,
@@ -178,6 +179,10 @@ func htmlFormat(s any, args ...any) template.HTML {
 		return htmlutil.HTMLFormat(v, args...)
 	}
 	panic(fmt.Sprintf("unexpected type %T", s))
+}
+
+func jsEscapeSafe(s string) template.HTML {
+	return template.HTML(template.JSEscapeString(s))
 }
 
 func queryEscape(s string) template.URL {

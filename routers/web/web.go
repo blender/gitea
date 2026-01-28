bdae4daf2ea26cf41c07b0a312040c17cb88e7ec
Oleg Komarov <oleg@blender.org>
2026-01-28 12:35:38 +0100

    BLENDER: contributor agreement support
    New models ContributorAgreement and SignedContributorAgreement are
    introduced.
    
    A new Settings > Contributor Agreements page is introduced.
    The page allows a user to review the text of existing contributor
    agreements and to sign any of them.
    
    A new /api/v1/users/{username}/contributor-agreements/{slug} endpoint
    allows to check if a user has signed a contributor agreement.
    This api endpoint can be used by a gitea action workflow as described in
    https://projects.blender.org/infrastructure/clacheck-runner
    
    SignContributorAgreement function searches for failed cla.yml workflows
    that were triggered by the current user and reruns them (search limited to
    10 latest workflow runs).
    Also "clacheck" statuses created by the clacheck webhook are updated.
    This updates the failed check status automatically and doesn't require any
    additional actions from the user.
    
    The runner code https://projects.blender.org/infrastructure/clacheck-runner
    automatically posts a pull request comment when a check fails:
    
        @username please sign the contributor agreement $CLA_SLUG at
        https://projects.blender.org/user/settings/contributor_agreements

--- routers/web/web.go
+++ routers/web/web.go
@@ -736,6 +736,9 @@ func registerWebRoutes(m *web.Router, webAuth *AuthMiddleware) {
 
 		// BLENDER: spam reporting
 		m.Post("/spamreport", user_setting.SpamReportUserPost)
+		// BLENDER: contributor agreement
+		m.Get("/contributor_agreements", user_setting.ContributorAgreements)
+		m.Post("/contributor_agreements/{slug}/sign", user_setting.SignContributorAgreement)
 	}, reqSignIn, user_setting.SettingsCtxData)
 
 	m.Group("/user", func() {
@@ -825,6 +828,12 @@ func registerWebRoutes(m *web.Router, webAuth *AuthMiddleware) {
 			m.Post("", admin.SpamReportsPost)
 		})
 		m.Post("/purge_spammer", admin.PurgeSpammerPost)
+
+		// BLENDER: contributor agreement
+		m.Group("/signed_contributor_agreements", func() {
+			m.Get("", admin.SignedContributorAgreements)
+			m.Combo("/batch_sign").Get(admin.ContributorAgreementsBatchSign).Post(admin.ContributorAgreementsBatchSignPost)
+		})
 
 		m.Group("/orgs", func() {
 			m.Get("", admin.Organizations)
